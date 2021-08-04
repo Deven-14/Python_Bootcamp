107be8bf6334ad08d27db9bc3c04803136d97feb
Deven-14 <deven.paramaj@gmail.com>
2021-08-04 23:24:32 +0530

    changed prompt and function names

--- Activity_15.py
+++ Activity_15.py
@@ -1,10 +1,10 @@
 
-def input_sentense():
-    list_of_strings = input("Enter a Sentense: ").split()
+def input_list_of_strings():
+    list_of_strings = input("Enter space separated strings: ").split()
     return list_of_strings
 
 if __name__ == "__main__":
-    list_of_strings = input_sentense()
+    list_of_strings = input_list_of_strings()
     list_of_strings.sort()
     new_list_of_strings = sorted(list_of_strings)
     print("Sorted list using sort(): ", list_of_strings)
