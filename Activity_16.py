eacd0f9cbf22cdc72292634640ebed846935a899
Deven-14 <deven.paramaj@gmail.com>
2021-08-04 23:27:00 +0530

    changed prompt and function name

--- Activity_16.py
+++ Activity_16.py
@@ -1,9 +1,9 @@
 
-def input_string():
-    string = input("Enter a string: ");
+def input_concatinated_string():
+    string = input("Enter a concatinated string: ");
     return string
 
 if __name__ == "__main__":
-    string = input_string()
+    string = input_concatinated_string()
     list_of_strings = [(*ele.split('='), ) for ele in string.split(';')]
     print(list_of_strings)
