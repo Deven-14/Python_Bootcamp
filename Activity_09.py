cff9fdd0f480c02e846bc00eb2ed5bf27eeb9848
Deven-14 <deven.paramaj@gmail.com>
2021-08-04 18:40:33 +0530

    removed class and used functions

--- Activity_09.py
+++ Activity_09.py
@@ -1,28 +1,20 @@
 import math
 
-class Tromboloid:
+def input_lenght_breadth_height():
+    length = float(input("Enter the length of the Tromboloid: "))
+    breadth = float(input("Enter the Breadth of the Tromboloid: "))
+    height = float(input("Enter the height of the Tromboloid: "))
+    return length, breadth, height
 
-    def __init__(self):
-        self.length = 0
-        self.breadth = 0
-        self.height = 0
-        self.characteristic_dimension = 0
-        self.volume = 0
+def calculate_volume(length, breadth, height):
+    temp = (breadth ** 2) * (height ** 2)
+    characteristic_dimension = length ** 2 + breadth ** 2 + height ** 2
+    volume = (temp / math.sqrt(characteristic_dimension))
+    return volume
 
-    def input_lenght_breadth_height(self):
-        self.length = float(input("Enter the length of the Tromboloid: "))
-        self.breadth = float(input("Enter the Breadth of the Tromboloid: "))
-        self.height = float(input("Enter the height of the Tromboloid: "))
-
-    def calculate_volume(self):
-        temp = (self.breadth ** 2) * (self.height ** 2)
-        self.characteristic_dimension = self.length ** 2 + self.breadth ** 2 + self.height ** 2
-        self.volume = (temp / math.sqrt(self.characteristic_dimension))
-        return self.volume
-
-    def output_volume(self):
-        print(f"The volume of the Tromboloid with length = {self.length:.3f}, breadth = {self.breadth:.3f}"
-              f" and height = {self.height:.3f} is = {self.volume:.3f}")
+def output_volume(length, breadth, height, volume):
+    print(f"The volume of the Tromboloid with length = {length:.3f}, breadth = {breadth:.3f}"
+          f" and height = {height:.3f} is = {volume:.3f}")
 
 def get_sphere_radius_given_volume(volume):
     temp = 3 * volume / 4 * math.pi
@@ -32,10 +24,9 @@ def output_sphere_raduis(radius):
     print(f"The radius of the sphere is = {radius:.3f}")
 
 if __name__ == "__main__":
-    t = Tromboloid()
-    t.input_lenght_breadth_height()
-    t.calculate_volume()
-    t.output_volume()
-    radius = get_sphere_radius_given_volume(t.volume)
+    length, breadth, height = input_lenght_breadth_height()
+    volume = calculate_volume(length, breadth, height)
+    output_volume(length, breadth, height, volume)
+    radius = get_sphere_radius_given_volume(volume)
     output_sphere_raduis(radius)
     
