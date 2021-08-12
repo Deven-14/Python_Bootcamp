d2a7a164d5ac8a4297e73ebd213a84d5b4f71247
Deven-14 <deven.paramaj@gmail.com>
2021-08-13 00:08:19 +0530

    made it simpler commit 1

--- Activity_22.py
+++ Activity_22.py
@@ -1,23 +1,17 @@
-from math import pi
+import math
 
 def input_angle():
     angle = int(input("Enter an angle in degrees: "))
-    return angle
-
-def degree_to_radian(angle):
-    return angle * pi / 180
-
-#def get_next_fact(start, stop, step):
-#    fact, num1, num2 = 1, 1, 1
-#    for ele in range(start, stop, step):
-#        yield fact
-#        fact *= (ele+1)*(ele+2)        
+    return angle      
 
 def sin(x):
-    num = 1
-    fact = (num := num*(i+1)*(i+2) for i in range(1, 16, 2))
-    elements = [x] + [x ** i / next(fact) for i in range(3, 16, 2)]
-    value = sum(elements[::2]) - sum(elements[1::2])
+    value, alternate = 0, 1
+    for i in range(1, 16, 2):
+        temp = x ** i / math.factorial(i) 
+        if alternate:
+            value, alternate = value+temp, 0
+        else:
+            value, alternate = value-temp, 1
     return value
 
 def output(x, sin_x):
@@ -25,5 +19,5 @@ def output(x, sin_x):
 
 if __name__ == "__main__":
     angle = input_angle()
-    sin_x = sin(degree_to_radian(angle))
+    sin_x = sin(math.radians(angle))
     output(angle, sin_x)
