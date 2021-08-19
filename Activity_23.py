0d4194adfff6fcca6dedc369c60e3a365f56ad71
Deven-14 <deven.paramaj@gmail.com>
2021-08-19 18:05:17 +0530

    done using itertools

--- Activity_23.py
+++ Activity_23.py
@@ -1,55 +1,35 @@
-from itertools import combinations_with_replacement as cwr
+from itertools import groupby, product, combinations_with_replacement as cwr
 
 def get_num_str():
     num_str = input("Enter a series of numbers: ").strip()
     return num_str
 
-def create_string_comb(comb):
-    string_comb = []
-
-    temp_comb = []
-    for ele in comb:
-        len_ = len(ele)
-        temp_comb.append([])
-                
-    return string_comb
-
 def get_combination(num_str):
     keypad = {'2': "ABC", '3': "DEF", '4': "GHI", '5': "JKL", '6': "MNO",
               '7': "PQRS", '8': "TUV", '9': "WXYZ"}
-
-    comb = [[] for _ in range(len(num_str))]
-    len_num_str = len(num_str)
-    i = 0
-
-    comb_len = []
-    prev_ele = ["", 0]
-    for i, ele in enumerate(num_str):
-        if prev_ele[0] == ele:
-            prev_ele[1] += 1
-        else:
-            prev_ele = [ele, 1]
-            comb_len.append(prev_ele)
-
-    print(comb_len)
-
-    temp_comb = []
-    for ele in comb_len:
-        temp = []
-        for i in range(ele[1]):
-            temp.append(["".join(t) for t in cwr(keypad[ele[0]][:ele[1]], ele[1]-i)])
-        temp_comb.append(temp)
-
-    print(temp_comb)
     
-    return comb_len
+    num_group = [(key, len(list(grp))) for key, grp in groupby(num_str)]
+    #print(num_group)
+    str_group = []
+    for ele, length in num_group:
+        rank = {key: index+1 for index, key in enumerate(keypad[ele])}
+        #print(rank)
+        temp = []
+        for i in range(1, length+1):
+            #print(*cwr(keypad[ele], i), i)
+            #for x in cwr(keypad[ele], i):
+            #    print(sum(rank[j] for j in x), i, x)
+            temp += [x for x in cwr(keypad[ele], i) if sum(rank[j] for j in x) == length]
+        str_group.append(temp)
 
-
-def output(combinations):
-    for ele in combinations:
-        print(ele, end=', ')
+    str_group = [[''.join(j) for j in i] for i in str_group]
+    comb = [''.join(x) for x in product(*str_group)]
+    #using while to remove the first ele where list len > 1 is wrong (1,2), (3,4), coz we even need 23 which will be lost if we removed so cartesian product
+    print(str_group)
+    print(comb)
+    return comb
 
 if __name__ == "__main__":
     num_str = get_num_str()
     comb = get_combination(num_str)
-    output(comb)
+    print(comb)
